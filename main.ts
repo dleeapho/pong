2be138a6ab293d9a265600318509fbeae31f7c44
Dan Leeaphon <dleapon@msn.com>
2023-03-06 05:40:26 -0600

    Refactor to avoid mixed return tuples by using global game-state vars.

--- main.ts
+++ main.ts
@@ -2,30 +2,35 @@ let bally: number;
 let returned: boolean;
 let position: number;
 let ballbright: number;
+let return_force: number;
 //  Simple pong
 let player = [Button.A, Button.B]
 let server = 0
 let receiver = 1
 let court = [0, 1, 2, 3, 4]
 let baseline = 3
-function serve(from_x: number, from_y: number): boolean[] {
-    /** serves a ball and returns True to indicate the game is now in play and resets the speed of the game */
-    let serving_speed = 10000
+//  Game state global vars
+let is_inplay = false
+let is_returned = false
+let rally_speed = 0
+function serve(from_x: number, from_y: number): number {
+    //  Serves a ball by putting is_inplay True and establishes the initial speed of the game
     let is_inplay = true
+    let serving_speed = 10000
     for (let setserve = 0; setserve < 3; setserve++) {
         pause(150)
         led.plotBrightness(from_x, from_y, 9)
         pause(150)
         led.plotBrightness(from_x, from_y, ballbright)
     }
-    return [is_inplay, serving_speed]
+    return serving_speed
 }
 
-function evaluate_turn(is_returned: boolean, position: number, current_speed: number, return_speed: number): boolean[] {
-    /** evaluates whether the rally is still in play and if so the new pace of the game */
+function evaluate_turn(position: number, current_speed: number, return_speed: number): number {
+    //  Evaluates whether the rally is still in play and if so the new pace of the game
     let is_inplay = true
     let speedup_factor = 3
-    //  smaller means returns speed up more quickly
+    //  Smaller here means returns speed up more quickly
     if (is_returned) {
         if (position == baseline) {
             current_speed = current_speed - Math.idiv(return_speed, speedup_factor)
@@ -46,13 +51,14 @@ function evaluate_turn(is_returned: boolean, position: number, current_speed: nu
         is_inplay = false
     }
     
-    return [is_inplay, current_speed]
+    return current_speed
 }
 
-function wait_for_hit(receivingplayer: number, speed: number): boolean[] {
+function wait_for_hit(receivingplayer: number, speed: number): number {
     let tick: number;
-    /** waits and indicates if the play has pressed a button and how long he/she waited */
-    let returned = false
+    let returned: boolean;
+    //  Waits and indicates if the play has pressed a button and how long the receiving player waited
+    let is_returned = false
     for (tick = 0; tick < speed; tick++) {
         if (input.buttonIsPressed(receivingplayer)) {
             returned = true
@@ -60,32 +66,30 @@ function wait_for_hit(receivingplayer: number, speed: number): boolean[] {
         }
         
     }
-    return [returned, tick]
+    return tick
 }
 
-let is_inplay = false
-let rally_speed = 0
 while (true) {
-    //  the rally loop
+    //  The rally loop
     bally = randint(0, 4)
     returned = false
     for (let ballx of court) {
-        //  the ball moving loop
+        //  The ball moving loop
         position = _py.py_array_index(court, ballx)
         ballbright = position + 5
         if (!is_inplay) {
-            let [is_inplay, rally_speed] = serve(ballx, bally)
+            rally_speed = serve(ballx, bally)
             led.plotBrightness(ballx, bally, ballbright)
         }
         
-        let [returned, return_force] = wait_for_hit(player[receiver], rally_speed)
+        return_force = wait_for_hit(player[receiver], rally_speed)
         led.plotBrightness(ballx, bally, 0)
-        if (returned) {
+        if (is_returned) {
             break
         }
         
     }
-    let [is_inplay, rally_speed] = evaluate_turn(returned, position, rally_speed, return_force)
+    rally_speed = evaluate_turn(position, rally_speed, return_force)
     // clear the button buffer
     input.buttonIsPressed(Button.A)
     input.buttonIsPressed(Button.B)
